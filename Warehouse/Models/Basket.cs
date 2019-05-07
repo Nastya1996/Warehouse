b0fdc25112479af115387e7fe70b6a54e1e6d92d
anihakobyan1998 <anihakobyan1710@gmail.com>
2019-05-07 17:16:41 +0400

    Create classes Basket,Unit, Product

---
+++ Warehouse/Models/Basket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Warehouse.Models
+{
+    public class Basket
+    {
+        public string Id { set; get; }
+        public string ProductManagerId { set; get; }
+        public string UserId { set; get; }
+    }
+}
---
+++ Warehouse/Models/Unit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Warehouse.Models
+{
+    public class Unit
+    {
+        public string Id { set; get; }
+        public string Name { set; get; }
+    }
+}
