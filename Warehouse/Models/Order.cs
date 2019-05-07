760f1c26aa08fd47124d6f834b49c95894eeb869
Yuri <avetisyan0326@gmail.com>
2019-05-07 17:13:58 +0400

    Add class [Type, Orders, ProductManager]

---
+++ Warehouse/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Warehouse.Models
+{
+    public class Order
+    {
+        public string OrderId { get; set; }
+        public uint Count { get; set; }
+        public decimal Sum { get; set; }
+        public DateTime Date { get; set; }
+        public string ProductId { get; set; }
+        public string CustomerId { get; set; }
+        public string UserId { get; set; }
+    }
+}
