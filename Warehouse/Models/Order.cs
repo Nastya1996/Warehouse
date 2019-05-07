529f0f6e5b727ee14f6fae506b44356edb03ce71
Yuri <avetisyan0326@gmail.com>
2019-05-07 18:17:41 +0400

    Addddd

--- Warehouse/Models/Order.cs
+++ Warehouse/Models/Order.cs
@@ -9,16 +9,12 @@ namespace Warehouse.Models
     public class Order
     {
         public string Id { get; set; }
-        public uint Count { get; set; }
         public decimal Sum { get; set; }
         public DateTime Date { get; set; }
-        public string ProductId { get; set; }
         public string CustomerId { get; set; }
         public string UserId { get; set; }
-        public Product Product { get; set; }
         public Customer Customer { get; set; }
         public IdentityUser<string> User { get; set; }
         public IList<ProductOrder> ProductOrders { get; set; }
-        
     }
 }
