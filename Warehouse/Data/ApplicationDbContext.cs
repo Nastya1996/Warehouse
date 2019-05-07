4841f09b3288ef2aea92d290ab69882599097e3d
Liya <liya.amirkhanian.m@gmail.com>
2019-05-07 17:39:37 +0400

    DbSeter

--- Warehouse/Data/ApplicationDbContext.cs
+++ Warehouse/Data/ApplicationDbContext.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Warehouse.Models;
 
 namespace Warehouse.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<ProductCustomer> ProductCustomers { get; set; }
+        public DbSet<WareHouse> Warehouses { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
