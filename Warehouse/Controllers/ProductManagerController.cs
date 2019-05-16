3559363d8dd359b66a9cfc985dfa4307104d89f4
Yuri <avetisyan0326@gmail.com>
2019-05-16 13:49:33 +0400

    Product Manager Controller

---
+++ Warehouse/Controllers/ProductManagerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Warehouse.Data;
+using Warehouse.Models;
+namespace Warehouse.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductManagerController(ApplicationDbContext context) => _context = context;
+        public IActionResult Index()
+        {
+            return View(_context.ProductManagers.Include(x=>x.WareHouse).ToList());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(ProductManager productManager)
+        {
+            _context.Add(productManager);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
