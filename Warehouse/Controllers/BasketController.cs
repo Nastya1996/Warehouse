e069d6e290a1aa1c4e4f0ec9fd7b7d4038d857ea
Liya <liya.amirkhanian.m@gmail.com>
2019-05-22 19:46:25 +0400

    basket index and delete

---
+++ Warehouse/Controllers/BasketController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Warehouse.Data;
+
+namespace Warehouse.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public BasketController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Baskets.Include(x=>x.ProductManager.Product.Unit).ToList());
+        }
+        public IActionResult Delete(string id)
+        {
+            return View(_context.Baskets.Find(id));
+        }
+        public IActionResult DeleteYes(string id)
+        {
+            _context.Baskets.Remove(_context.Baskets.Find(id));
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
