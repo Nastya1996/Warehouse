3a0ea01cd0d22423e6f52e6917f209b38ddb4623
Yuri <avetisyan0326@gmail.com>
2019-06-06 12:27:29 +0400

    Change

--- Warehouse/wwwroot/js/user.js
+++ Warehouse/wwwroot/js/user.js
@@ -1,40 +1,39 @@
 ﻿$(() => {
-    //Disable and Enable product
     $("[href='#']").on("click", function () {
         let $link = $(this);
         let $row = $(this).parent().parent().addClass(".select");
-        let productId = $row.children().children().val();
-        if ($link.text() === 'Enable') {
+        let userId = $row.children().children().val();
+        if ($link.text() === "Enable") {
             $.ajax({
-                url: '/Products/Enable',
+                url: '/User/Enable',
                 type: "POST",
-                data: JSON.stringify(productId),
+                data: JSON.stringify(userId),
                 contentType: "application/json; charset=utf-8",
                 dataType: "json",
                 async: false,
                 success: function (data) {
                     if (data) {
-                        $("#prodText").text("Product has been activated!");
-                        $("#prodModal").modal();
-                        $link.text("Disable").css("color","red");
+                        $("#userText").text("User has been activated!");
+                        $("#userModal").modal();
+                        $link.text("Disable").css("color", "red");
                         $row.removeClass();
                     }
                 }
-            })
+            });
         }
         else if ($link.text() === 'Disable') {
             $.ajax({
-                url: '/Products/Disable',
+                url: '/User/Disable',
                 type: "POST",
-                data: JSON.stringify(productId),
+                data: JSON.stringify(userId),
                 contentType: "application/json; charset=utf-8",
                 dataType: "json",
                 async: false,
                 success: function (data) {
                     if (data) {
-                        $("#prodText").text("Product has been deactivated!");
-                        $("#prodModal").modal();
-                        $link.text("Enable").css("color","green");
+                        $("#userText").text("User has been deactivated!");
+                        $("#userModal").modal();
+                        $link.text("Enable").css("color", "green");
                         $row.removeClass();
                     }
                 }
